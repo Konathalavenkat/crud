f1e5c51371a7b9ba4185e17f0cf935cbd1e07dee
venkat <venkatsivaff@gmail.com>
2023-10-25 11:56:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose')
+const express = require('express');
+
+const bodyparser = require('body-parser')
+const cors = require('cors')
+
+const studentRoute = require('./controller/studentRoute');
+mongoose.set("strictQuery", true)
+mongoose.connect("mongodb+srv://venkat:chopper@cluster0.uekukzo.mongodb.net/schooldb")
+
+var app = express();
+
+let db = mongoose.connection
+
+db.on("open", () => {console.log("Connected to db")})
+db.on("error", () => {console.log("Failed to connect to db")})
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(cors());
+
+
+app.use("/studentdetails", studentRoute)
+
+app.listen(4000, () => {
+    console.log("Server started at port 4000")
+})
